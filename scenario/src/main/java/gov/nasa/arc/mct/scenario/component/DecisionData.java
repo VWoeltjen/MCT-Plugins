9a453537bb7cae0ca06b7f3ac4e7807dfb5c1252
nshi <nija.shi@nasa.gov>
2013-05-23 11:54:05 -0700

    Enabled editing of decision component in the info view; render decisions in activity timeline; show time in DOY.

---
+++ scenario/src/main/java/gov/nasa/arc/mct/scenario/component/DecisionData.java
@@ -0,0 +1,32 @@
+package gov.nasa.arc.mct.scenario.component;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class DecisionData {
+
+	private Date startDate;
+	private Date endDate;
+
+	public Date getStartTime() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndTime() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+}
