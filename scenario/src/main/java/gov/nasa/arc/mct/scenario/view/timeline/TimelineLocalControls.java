81796bc9a4c2d5f55c6ad999afc8e902483dd83a
Victor Woeltjen <victor.woeltjen@nasa.gov>
2013-06-26 15:22:31 -0700

    Added numbered tick marks to lower timeline controls

--- scenario/src/main/java/gov/nasa/arc/mct/scenario/view/timeline/TimelineLocalControls.java
+++ scenario/src/main/java/gov/nasa/arc/mct/scenario/view/timeline/TimelineLocalControls.java
@@ -26,11 +26,16 @@ import gov.nasa.arc.mct.scenario.component.DurationCapability;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
+import java.awt.RenderingHints;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TimeZone;
 
 import javax.swing.BorderFactory;
@@ -50,10 +55,27 @@ public class TimelineLocalControls extends JPanel implements DurationCapability 
 	public static final int RIGHT_MARGIN = 12;
 	
 	private static final int TICK_AREA_HEIGHT = 40;
+
+	private static final long[] TICK_DIVISIONS = // These get multiplied in static block below
+		{1, 10, 10, 10, // Up to 1000 ms -> 1s
+		 5, 3,  2,  2,  // Up to   60 s  -> 1m
+		 5, 3,  2,  2,  // Up to   60 m  -> 1h
+		 3, 2,  2,  2,  // Up to   24 h  -> 1d
+		 7,             // Up to    7 d  -> 1w
+		};
+	private static final Map<Long, String> NAMED_TICK_SIZES = new HashMap<Long, String>();
 	
 	public static final DateFormat DURATION_FORMAT = new SimpleDateFormat("HH:mm");
 	static {
 		DURATION_FORMAT.setTimeZone(TimeZone.getTimeZone("GMT"));
+		for (int i = 1; i < TICK_DIVISIONS.length; i++) {
+			TICK_DIVISIONS[i] *= TICK_DIVISIONS[i-1];
+		}
+		NAMED_TICK_SIZES.put(1000L, "Seconds");
+		NAMED_TICK_SIZES.put(1000L * 60L, "Minutes");
+		NAMED_TICK_SIZES.put(1000L * 60L * 60L, "Hours");
+		NAMED_TICK_SIZES.put(1000L * 60L * 60L * 24L, "Days");
+		NAMED_TICK_SIZES.put(1000L * 60L * 60L * 24L * 7L, "Weeks");		
 	}
 	
 	private static final long serialVersionUID = 5844637696012429283L;
@@ -69,6 +91,8 @@ public class TimelineLocalControls extends JPanel implements DurationCapability 
 	private JSlider zoomControl;
 	private JLabel durationLabel;
 	
+	private JLabel timeLabel;
+	
 	public TimelineLocalControls(DurationCapability masterDuration) {
 		super(new BorderLayout());
 		this.masterDuration = masterDuration;
@@ -77,6 +101,7 @@ public class TimelineLocalControls extends JPanel implements DurationCapability 
 		add(upperPanel = makeUpperPanel(), BorderLayout.NORTH);
 		add(contentPane, BorderLayout.CENTER);
 		add(lowerPanel = makeLowerPanel(), BorderLayout.SOUTH);
+		updateLabels();
 		
 		this.addAncestorListener(new AncestorListener() {
 			@Override
@@ -108,6 +133,9 @@ public class TimelineLocalControls extends JPanel implements DurationCapability 
 		return contentPane;
 	}
 	
+	
+	
+	
 	private JComponent makeUpperPanel() {
 		JPanel upperPanel = new JPanel(new BorderLayout());
 		
@@ -132,8 +160,8 @@ public class TimelineLocalControls extends JPanel implements DurationCapability 
 		SpringLayout springLayout = new SpringLayout();
 		JPanel lowerPanel = new JPanel(springLayout);
 		
-		JLabel hoursLabel = new JLabel("Hours");
-		JPanel tickPanel = new JPanel();
+		timeLabel = new JLabel("Hours");
+		JPanel tickPanel = new TickMarkPanel();
 		JButton leftButton = new JButton(new PanIcon(-1));
 		leftButton.setOpaque(false);
         leftButton.setContentAreaFilled(false);
@@ -142,10 +170,10 @@ public class TimelineLocalControls extends JPanel implements DurationCapability 
 		rightButton.setOpaque(false);
         rightButton.setContentAreaFilled(false);
         rightButton.setBorder(BorderFactory.createEmptyBorder());
-		tickPanel.setBackground(Color.ORANGE);
+		tickPanel.setOpaque(false);
 		
 		
-		lowerPanel.add(hoursLabel);
+		lowerPanel.add(timeLabel);
 		lowerPanel.add(tickPanel);
 		lowerPanel.add(leftButton);
 		lowerPanel.add(rightButton);
@@ -155,18 +183,30 @@ public class TimelineLocalControls extends JPanel implements DurationCapability 
 		
 		springLayout.putConstraint(SpringLayout.EAST, leftButton, 0, SpringLayout.WEST, tickPanel);
 		springLayout.putConstraint(SpringLayout.WEST, rightButton, 0, SpringLayout.EAST, tickPanel);
-		springLayout.putConstraint(SpringLayout.EAST, hoursLabel, 0, SpringLayout.WEST, leftButton);
+		springLayout.putConstraint(SpringLayout.EAST, timeLabel, 0, SpringLayout.WEST, leftButton);
 		
 		springLayout.putConstraint(SpringLayout.SOUTH, lowerPanel, TICK_AREA_HEIGHT, SpringLayout.NORTH, lowerPanel);
 		springLayout.putConstraint(SpringLayout.SOUTH, tickPanel, 0, SpringLayout.SOUTH, lowerPanel);
 		springLayout.putConstraint(SpringLayout.NORTH, tickPanel, 0, SpringLayout.NORTH, lowerPanel);
 		
-		springLayout.putConstraint(SpringLayout.VERTICAL_CENTER, hoursLabel, 0, SpringLayout.VERTICAL_CENTER, lowerPanel);
+		springLayout.putConstraint(SpringLayout.VERTICAL_CENTER, timeLabel, 0, SpringLayout.VERTICAL_CENTER, lowerPanel);
 		springLayout.putConstraint(SpringLayout.VERTICAL_CENTER, leftButton, 0, SpringLayout.VERTICAL_CENTER, lowerPanel);
 		springLayout.putConstraint(SpringLayout.VERTICAL_CENTER, rightButton, 0, SpringLayout.VERTICAL_CENTER, lowerPanel);
 		
+		lowerPanel.setBackground(EDGE_COLOR);
+		lowerPanel.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, EDGE_COLOR.darker()));
+		
 		return lowerPanel;
 	}	
+	
+	private void updateLabels() {
+		String label = NAMED_TICK_SIZES.get(findNamedTickSize());
+		if (label == null) {
+			label = "???"; // TODO: Log
+		}
+		timeLabel.setText(label);
+		durationLabel.setText("Total Duration: " + DURATION_FORMAT.format(new Date(masterDuration.getEnd() - masterDuration.getStart())));
+	}
 	
 	@Override
 	public long getStart() {
@@ -207,11 +247,76 @@ public class TimelineLocalControls extends JPanel implements DurationCapability 
 		return TimelineLocalControls.RIGHT_MARGIN;
 	}
 	
+	// Utility functions to pick out meaningful tick sizes
+	private int findTickSizeIndex(long sz) {
+		int i = 0;
+		while (i < TICK_DIVISIONS.length && TICK_DIVISIONS[i] < sz) {
+			i++;
+		}
+		return i;
+	}
+
+	private long findNamedTickSize() {
+		long longest = 1;
+		long duration = getEnd() - getStart();
+		for (Long sz : NAMED_TICK_SIZES.keySet()) {
+			if (sz < duration && sz > longest) {
+				longest = sz;
+			}
+		}
+		return longest;
+	}
 	
 	private class TickMarkPanel extends JPanel {
-		private TickMarkPanel() {
+		private static final long serialVersionUID = -3582412138892442244L;
+
+		public void paintComponent (Graphics g) {
+			int tickHeight = TICK_AREA_HEIGHT / 3;
+			int tickBottom = tickHeight * 2;
+			long largeTickSize = findNamedTickSize();
+			int tickSizeIndex = findTickSizeIndex(largeTickSize);
 			
-		}
+			// Enable smooth rendering, since ticks might alias to pixels unevenly
+			if (g instanceof Graphics2D) {
+				RenderingHints renderHints = new RenderingHints(
+						RenderingHints.KEY_ANTIALIASING,
+						RenderingHints.VALUE_ANTIALIAS_ON);
+				renderHints.put(RenderingHints.KEY_RENDERING,
+						RenderingHints.VALUE_RENDER_QUALITY);
+				((Graphics2D) g).setRenderingHints(renderHints);
+			}
+			
+			// Draw ticks
+			long startTime = getStart();
+			long endTime = getEnd();
+			g.setColor(Color.BLACK);
+			g.setFont(g.getFont().deriveFont(8.0f));
+			FontMetrics metrics = g.getFontMetrics(g.getFont());
+			int ascent = metrics.getAscent();
+			while (tickSizeIndex >= 0 && tickHeight > 2) {
+				long tickInterval = TICK_DIVISIONS[tickSizeIndex];
+				
+				// Don't bunch together too many ticks
+				if (tickInterval * getPixelScale() < 2.0) break;
+				
+				long tickStart = (startTime - startTime % tickInterval) + tickInterval;				
+				for (long tick = tickStart; tick < endTime; tick += tickInterval) {
+					int x = (int) ((tick - getTimeOffset()) * getPixelScale());
+					g.drawLine(x, tickBottom - tickHeight, x, tickBottom);
+					
+					// Label largest tick marks only
+					if (tickInterval == largeTickSize) {
+						String tickNumber = Long.toString(tick / tickInterval);
+						g.drawString(tickNumber, x - metrics.stringWidth(tickNumber) / 2, tickBottom + ascent + 2);
+					}
+				}
+				
+				
+				// Shrink subsequent ticks
+				tickHeight = (tickHeight * 3) / 4;
+				tickSizeIndex--;
+			}
+		}		
 	}
 	
 	private class PanIcon implements Icon {
@@ -246,4 +351,5 @@ public class TimelineLocalControls extends JPanel implements DurationCapability 
 		}
 		
 	}
+	
 }
