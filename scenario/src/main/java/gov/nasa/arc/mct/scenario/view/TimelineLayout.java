7dac63dea6db37b5b0460db292b6c4533a207408
Victor Woeltjen <victor.woeltjen@nasa.gov>
2013-09-03 14:48:54 -0700

    [Scenario] Add comments/license to timeline layout

--- scenario/src/main/java/gov/nasa/arc/mct/scenario/view/TimelineLayout.java
+++ scenario/src/main/java/gov/nasa/arc/mct/scenario/view/TimelineLayout.java
@@ -1,3 +1,24 @@
+/*******************************************************************************
+ * Mission Control Technologies, Copyright (c) 2009-2012, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space 
+ * Administration. All rights reserved.
+ *
+ * The MCT platform is licensed under the Apache License, Version 2.0 (the 
+ * "License"); you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ *
+ * MCT includes source code licensed under additional open source licenses. See 
+ * the MCT Open Source Licenses file included with this distribution or the About 
+ * MCT Licenses dialog available at runtime from the MCT Help menu for additional 
+ * information. 
+ *******************************************************************************/
 package gov.nasa.arc.mct.scenario.view;
 
 import gov.nasa.arc.mct.scenario.component.DurationCapability;
@@ -21,6 +42,15 @@ import java.util.Set;
 
 import javax.swing.Timer;
 
+/**
+ * Lays out objects in a timeline (more specifically, in one block of a timeline)
+ * This layout is responsible for positioning components horizontally to 
+ * reflect their start/end times, and for shifting components vertically 
+ * (and animating this transition) to prevent them from overlapping.
+ * 
+ * @author vwoeltje
+ *
+ */
 public class TimelineLayout implements LayoutManager2 {
 	private int rowHeight = 24;
 	private int rowPadding = 6;
