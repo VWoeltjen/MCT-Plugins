b360a4b45cc48d21ba33a7e11e140c8b95b5c7d3
Victor Woeltjen <victor.woeltjen@nasa.gov>
2013-09-03 13:48:10 -0700

    [Scenario] Improve timeline layout performance

--- scenario/src/main/java/gov/nasa/arc/mct/scenario/view/TimelineLayout.java
+++ scenario/src/main/java/gov/nasa/arc/mct/scenario/view/TimelineLayout.java
@@ -16,6 +16,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -31,17 +32,34 @@ public class TimelineLayout implements LayoutManager2 {
 	private int rowHeight = 24;
 	private int rowPadding = 6;
 	private TimelineContext context;
-	private List<List<Component>> rows = new ArrayList<List<Component>>();
+	private List<List<ComponentInfo>> rows = new ArrayList<List<ComponentInfo>>();
 	
-	// TODO: Maybe scrunch some/all of these together into same data structure?
-	private Map<Component, DurationCapability> durationInfo = new HashMap<Component, DurationCapability>();
-	private Map<Component, Integer> rowMap = new HashMap<Component, Integer>();
-	private Map<Component, Float> animation = new HashMap<Component, Float>();
-	private Map<Component, Integer> orderAdded = new HashMap<Component, Integer>();	
+	private Map<Component, ComponentInfo> componentInfo = new HashMap<Component, ComponentInfo>();
+	private Set<ComponentInfo> animating = new HashSet<ComponentInfo>();
 	
-	private Timer animator = null;
-	
+	private Timer animator = null;	
 	private boolean rowAssignmentsHandled = false;
+
+	private static class ComponentInfo {
+		// Start and end times
+		private DurationCapability durationCapability;
+		
+		// Current row for the component
+		private int row = 0;
+		
+		// Current animation (vertical offset, in rows) for component
+		private float animation = 0f;
+		
+		// Order in which component was added
+		private int order;
+
+		public ComponentInfo(DurationCapability durationCapability, int order) {
+			super();
+			this.durationCapability = durationCapability;
+			this.order = order;
+		}
+	}
+	
 	
 	public TimelineLayout(TimelineContext context) {
 		super();
@@ -54,21 +72,13 @@ public class TimelineLayout implements LayoutManager2 {
 
 	@Override
 	public void removeLayoutComponent(Component comp) {
-		if (durationInfo.containsKey(comp)) {
-			durationInfo.remove(comp);
+		if (componentInfo.containsKey(comp)) {
+			int row = componentInfo.get(comp).row;
+			rows.get(row).remove(comp);
+			animating.remove(componentInfo.get(comp));
+			componentInfo.remove(comp);
 		}
-		if (rowMap.containsKey(comp)) {
-			rows.get(rowMap.get(comp)).remove(comp);
-		}
-		for (int i = 0 ; i < rows.size() ; i++) {
-			cleanupRow(i);
-		}
-		for (int i = 1 ; i < rows.size() ; i++) {
-			packRow(i);
-		}
-		if (orderAdded.containsKey(comp)) {
-			orderAdded.remove(comp);
-		}
+		// TODO: Adjust other orders
 	}
 
 	@Override
@@ -89,32 +99,37 @@ public class TimelineLayout implements LayoutManager2 {
 		
 		// Lay out components temporally
 		for (Component child : parent.getComponents()) {
-			DurationCapability duration = durationInfo.get(child);
-			if (duration != null) {
-				int x = context.getLeftPadding() + (int) (context.getPixelScale() * (double) (duration.getStart() - context.getTimeOffset()));
-				int width = (int) (context.getPixelScale() * (double) (duration.getEnd() - duration.getStart())) + 1;
+			ComponentInfo info = componentInfo.get(child);
+			if (componentInfo != null) {
+				int x = context.getLeftPadding() + (int) (context.getPixelScale() * (double) (info.durationCapability.getStart() - context.getTimeOffset()));
+				int width = (int) (context.getPixelScale() * (double) (info.durationCapability.getEnd() - info.durationCapability.getStart())) + 1;
 				
 				// Vertical offset due to row change animation
-				int animationOffset = animation.containsKey(child) ? 
-							(int) (fullRowHeight * animation.get(child)) : 0;
+				int animationOffset = (int) (fullRowHeight * info.animation);
 				
-				child.setBounds(x, getRow(child) * fullRowHeight + animationOffset + rowPadding/2, width, rowHeight);					
+				child.setBounds(x, info.row * fullRowHeight + animationOffset + rowPadding/2, width, rowHeight);					
 			}
 		}
 		
 		// Start animating if needed
-		if (!animation.isEmpty() && animator == null) {
+		if (!animating.isEmpty() && animator == null) {
 			animator = new Timer(25, new ActionListener() {
 				@Override
-				public void actionPerformed(ActionEvent arg0) {
-					Map<Component, Float> nextAnimation = new HashMap<Component, Float>();
-					for (Entry<Component, Float> entry : animation.entrySet()) {
-						if (Math.abs(entry.getValue()) > 1.0f / ((float)rowHeight)) {
-							float next = entry.getValue() * 0.75f;
-							nextAnimation.put(entry.getKey(), next);							
+				public void actionPerformed(ActionEvent evt) {
+					Iterator<ComponentInfo> iter = animating.iterator();
+					while (iter.hasNext()) {
+						ComponentInfo component = iter.next();
+						component.animation *= 0.75f;
+						if (Math.abs(component.animation) <  1.0f / ((float)rowHeight)) {
+							component.animation = 0f;
+							iter.remove();
 						}
 					}		
-					animation = nextAnimation;
+					
+					if (animating.isEmpty()) {
+						animator.stop();
+						animator = null;
+					}
 					
 					// Update layout for parents, since height may have changed
 					// TODO: Only do this if height will change?
@@ -136,13 +151,15 @@ public class TimelineLayout implements LayoutManager2 {
 	@Override
 	public void addLayoutComponent(Component comp, Object constraints) {
 		if (constraints instanceof DurationCapability) {
-			durationInfo.put(comp, (DurationCapability) constraints);
+			ComponentInfo info = new ComponentInfo((DurationCapability) constraints, componentInfo.size());
+			componentInfo.put(comp, info);
+
 			// Find a row with room for this component
 			int row = 0;
 			while (row < rows.size()) {
 				boolean fits = true;
-				for (Component c : rows.get(row)) {
-					if (overlaps(c,comp)) {
+				for (ComponentInfo c : rows.get(row)) {
+					if (overlaps(c,info)) {
 						fits = false;
 						break;
 					}
@@ -153,20 +170,10 @@ public class TimelineLayout implements LayoutManager2 {
 					row++;
 				}
 			}
-			orderAdded.put(comp, orderAdded.size());
-			setRow(comp, row, false); // Will get sorted during layout
+			setRow(info, row, false); // Will get sorted during layout
 		} else {
 			throw new IllegalArgumentException("Only valid constraint for " + getClass().getName() + 
 					" is " + DurationCapability.class.getName());
-		}
-		for (List<Component> row : rows) {
-			Collections.sort(row, comparator);
-		}
-		for (int i = 0 ; i < rows.size() ; i++) {
-			cleanupRow(i);
-		}
-		for (int i = 1 ; i < rows.size() ; i++) {
-			packRow(i);
 		}
 	}
 
@@ -192,7 +199,7 @@ public class TimelineLayout implements LayoutManager2 {
 	private void handleRowAssignments() {
 		if (!rowAssignmentsHandled) {
 			// Fix row assignments
-			for (List<Component> row : rows) {
+			for (List<ComponentInfo> row : rows) {
 				Collections.sort(row, comparator);
 			}
 			for (int i = 0; i < rows.size(); i++) {
@@ -200,11 +207,6 @@ public class TimelineLayout implements LayoutManager2 {
 			}
 			for (int i = 1; i < rows.size(); i++) {
 				packRow(i);
-			}
-	
-			// Remove empty rows
-			while (!rows.isEmpty() && rows.get(rows.size() - 1).isEmpty()) {
-				rows.remove(rows.size() - 1);
 			}
 			
 			rowAssignmentsHandled = true;
@@ -223,85 +225,83 @@ public class TimelineLayout implements LayoutManager2 {
 			bestRow--;
 		}
 		int desiredHeight = (bestRow + 1) * fullRowHeight;
-		for (Entry<Component, Float> entry : animation.entrySet()) {
+		for (ComponentInfo info : animating) {
 			int necessaryHeight = 
-					(int) (((float) rowMap.get(entry.getKey()) + entry.getValue() + 1) * fullRowHeight);   
+					(int) (((float) info.row + info.animation + 1) * fullRowHeight);   
 			desiredHeight = Math.max(desiredHeight, necessaryHeight);
 		}		
 		return desiredHeight;
 	}
 	
 	private boolean containsUnanimatedComponents(int row) {
-		for (Component c : rows.get(row)) {
-			if (!animation.containsKey(c)) {
+		for (ComponentInfo c : rows.get(row)) {
+			if (!animating.contains(c)) {
 				return true;
 			}				
 		}
 		return false;
 	}
-		
-	private int getRow(Component comp) {
-		Integer row = rowMap.get(comp);
-		return row == null ? 0 : row;
-	}
-	
-	private void setRow(Component comp, int row, boolean animated) {
-		int original = -1; // Used to distinguish if there WAS an original row
-		
-		if (rowMap.containsKey(comp)) {
-			original = rowMap.get(comp);
-			rows.get(rowMap.get(comp)).remove(comp);
-		}
-		rowMap.put(comp, row);
+			
+	private void setRow(ComponentInfo info, int row, boolean animated) {
+		// This will be -1 by default, to indicate "unset"
+		int original = info.row;
+		info.row = row;
 		
 		while (rows.size() <= row) {
-			rows.add(new ArrayList<Component>());
+			rows.add(new ArrayList<ComponentInfo>());
 		}
-		rows.get(row).add(comp);
+		if (rows.size() > original) {
+			rows.get(original).remove(info);
+		}
+		rows.get(row).add(info);
 		
-		if (original >= 0 && original != row) {
-			float diff = (float) (original - row);
-			if (animation.containsKey(comp)) {
-				diff += animation.get(comp);
-			}
-			animation.put(comp, diff);
+		if (original != row && animated) {
+			info.animation += (float) (original - row);			
+			animating.add(info);
 		}
 	}
 	
-	private List<Component> fixList = new ArrayList<Component>();
+	private List<ComponentInfo> fixList = new ArrayList<ComponentInfo>();
 	
 	private void cleanupRow(int row) {
 		Set<Component> active = context.getActiveViews();
-		List<Component> comps = rows.get(row);
+		List<ComponentInfo> comps = rows.get(row);
 		
 		// Identify comps which should be pushed down a row
 		for (int i = 0; i < comps.size(); i++) {
-			Component c1 = comps.get(i);
+			ComponentInfo c1 = comps.get(i);
 			for (int j = i + 1; j < comps.size(); j++) {
-				Component c2 = comps.get(j);
+				ComponentInfo c2 = comps.get(j);
 				if (!c1.equals(c2)) {
-					Component toMove = toMove(c1, c2);
+					ComponentInfo toMove = toMove(c1, c2);
 					if (toMove != null) {
 						if (!active.contains(toMove)) {
 							fixList.add(toMove);
 						} else {
 							fixList.add(toMove.equals(c1) ? c2 : c1);
 						}
+					} else {
+						break; // c1 is clear
 					}
 				}
 			}
 		}
 		
 		// Push them
-		for (Component c : fixList) {
+		for (ComponentInfo c : fixList) {
 			setRow(c, row + 1, true);
+		}
+		
+		// Make sure row is sorted before subsequent cleanupRow
+		if (!fixList.isEmpty()) {
+			Collections.sort(rows.get(row + 1), comparator);
 		}
 		
 		// Clear out the list to reuse later
 		fixList.clear();
 	}
 	
-	private Map<Component, Integer> fixMap = new HashMap<Component, Integer>();
+	private Map<ComponentInfo, Integer> fixMap = new HashMap<ComponentInfo, Integer>();
 	
 	/*
 	 * We assume cleanupRow has already been called for all rows, 
@@ -309,77 +309,62 @@ public class TimelineLayout implements LayoutManager2 {
 	 */
 	private void packRow(int row) {
 		Set<Component> active = context.getActiveViews();
-		List<Component> current = rows.get(row);
-		for (Component c1 : current) {
+		List<ComponentInfo> current = rows.get(row);
+		for (ComponentInfo c1 : current) {
 			if (!active.contains(c1)) {
-				for (int other = row - 1; other >= 0; other--) {
-					List<Component> above = rows.get(other);
+				outer: for (int other = row - 1; other >= 0; other--) {
+					List<ComponentInfo> above = rows.get(other);
 
-					boolean fits = true;
-					for (Component c2 : above) {
+					for (ComponentInfo c2 : above) {
 						if (c1 != c2 && overlaps(c1, c2)) {
-							fits = false;
-							break;
+							break outer;
 						}
 					}
-					if (fits) {
-						fixMap.put(c1, other);
-					} else {
-						break;
-					}
+					
+					// If control reaches here, this fits
+					fixMap.put(c1, other);
 				}
 			}
 		}
 
-		for (Entry<Component, Integer> fix : fixMap.entrySet()) {
+		for (Entry<ComponentInfo, Integer> fix : fixMap.entrySet()) {
 			setRow(fix.getKey(), fix.getValue(), true);
 		}
 		fixMap.clear();
 
 	}
 	
-	private Component toMove(Component c1, Component c2) {
+	private ComponentInfo toMove(ComponentInfo c1, ComponentInfo c2) {
+		if (!overlaps(c1, c2)) {
+			return null;
+		}
+		
 		// Prefer to move the most recently-added component downward
-		Integer o1 = orderAdded.get(c1);
-		Integer o2 = orderAdded.get(c2);
-		if (o1 < o2) {
+		if (c1.order < c2.order) {
 			return c2;
-		} else if (o2 < o1) {
+		} else if (c2.order < c1.order) {
 			return c1;
 		}
 		
 		// Probably unreachable, but fall back to moving larger activities
 		// if that is necessary
-		DurationCapability d1 = durationInfo.get(c1);
-		DurationCapability d2 = durationInfo.get(c2);
-		if (d1 != null && d2 != null) {
-			if (d1.getEnd() > d2.getStart() && d2.getEnd() > d1.getStart()) {
-				// Suggest that the smaller one be moved
-				return (d1.getEnd() - d1.getStart() > d2.getEnd() - d2.getStart()) ?
-						c2 : c1;
-			}
-		}		
-		return null;
+		DurationCapability d1 = c1.durationCapability;
+		DurationCapability d2 = c2.durationCapability;
+		return (d1.getEnd() - d1.getStart() > d2.getEnd() - d2.getStart()) ? c2 : c1;
 	}
 	
-	private boolean overlaps(Component c1, Component c2) {
-		DurationCapability d1 = durationInfo.get(c1);
-		DurationCapability d2 = durationInfo.get(c2);
-		if (d1 != null && d2 != null) {
-			return (d1.getEnd() > d2.getStart() && d2.getEnd() > d1.getStart());
-		}		
-		return false;
+	private boolean overlaps(ComponentInfo c1, ComponentInfo c2) {
+		DurationCapability d1 = c1.durationCapability;
+		DurationCapability d2 = c2.durationCapability;
+		return (d1.getEnd() > d2.getStart() && d2.getEnd() > d1.getStart());
 	}
 	
-	private final Comparator<Component> comparator = new Comparator<Component>() {
+	private static final Comparator<ComponentInfo> comparator = new Comparator<ComponentInfo>() {
 		@Override
-		public int compare(Component c1, Component c2) {
-			DurationCapability d1 = durationInfo.get(c1);
-			DurationCapability d2 = durationInfo.get(c2);
-			if (d1 != null && d2 != null) {
-				return Long.valueOf(d1.getStart()).compareTo(Long.valueOf(d2.getStart()));
-			}
-			return 0;
+		public int compare(ComponentInfo c1, ComponentInfo c2) {
+			long s1 = c1.durationCapability.getStart();
+			long s2 = c2.durationCapability.getStart();
+			return (int) Math.signum(s1 - s2);
 		}
 		
 	};
@@ -397,7 +382,7 @@ public class TimelineLayout implements LayoutManager2 {
 		layout.context = layout.mouseAdapter;
 		
 		final JPanel panel = new JPanel(layout);
-		for (int i = 0; i < 200; i++) {
+		for (int i = 0; i < 1000; i++) {
 			int start = (int) (Math.random() * 1150.0);
 			int end   = start + 5 + (100 - i/10) * (int) (1.0 + Math.random());
 			DurationThing thing = layout.new DurationThing(start,end);
@@ -486,12 +471,12 @@ public class TimelineLayout implements LayoutManager2 {
 			return activeView;
 		}		
 	};
-	
+
+	static int i = 0;
 	private class DurationThing extends JLabel implements DurationCapability {
 		long start, end;
-
 		public DurationThing(long start, long end) {
-			super("THING");
+			super("THING " + i++);
 			setBorder(BorderFactory.createLineBorder(Color.BLUE));
 			this.start = start;
 			this.end = end;
