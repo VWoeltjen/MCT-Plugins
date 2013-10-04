6c09153ab266d1f7227e2fed58b8989a0b742489
Victor Woeltjen <victor.woeltjen@nasa.gov>
2013-10-04 08:08:14 -0700

    [Scenario] Add persistence support to tag repository

--- scenario/src/main/java/gov/nasa/arc/mct/scenario/component/TagRepositoryComponent.java
+++ scenario/src/main/java/gov/nasa/arc/mct/scenario/component/TagRepositoryComponent.java
@@ -1,9 +1,22 @@
 package gov.nasa.arc.mct.scenario.component;
 
 import gov.nasa.arc.mct.components.AbstractComponent;
+import gov.nasa.arc.mct.components.JAXBModelStatePersistence;
+import gov.nasa.arc.mct.components.ModelStatePersistence;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 
 public class TagRepositoryComponent extends AbstractComponent implements RepositoryCapability {
-	private String scope;
+	private TagRepositoryModel model;
+	
+	@Override
+	public <T> T handleGetCapability(Class<T> capabilityClass) {
+		return (capabilityClass.isAssignableFrom(ModelStatePersistence.class)) ?
+			capabilityClass.cast(persistence) :
+			super.handleGetCapability(capabilityClass);
+	}
 	
 	@Override
 	public Class<?> getCapabilityClass() {
@@ -12,6 +25,30 @@ public class TagRepositoryComponent extends AbstractComponent implements Reposit
 
 	@Override
 	public String getUserScope() {
-		return scope;
+		return model.scope;
 	}
+	
+	@XmlRootElement
+	@XmlAccessorType(XmlAccessType.FIELD)
+	public static class TagRepositoryModel {
+		private String scope;
+	}
+	
+	private final ModelStatePersistence persistence = 
+			new JAXBModelStatePersistence<TagRepositoryModel>() {
+		@Override
+		protected TagRepositoryModel getStateToPersist() {
+			return model;
+		}
+
+		@Override
+		protected void setPersistentState(TagRepositoryModel modelState) {
+			model = (TagRepositoryModel) modelState;
+		}
+
+		@Override
+		protected Class<TagRepositoryModel> getJAXBClass() {
+			return TagRepositoryModel.class;
+		}		
+	};
 }
