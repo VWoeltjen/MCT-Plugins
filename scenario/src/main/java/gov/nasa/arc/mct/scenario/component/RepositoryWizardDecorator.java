4b85120b4a0ef50a2a0183cf2dd0e6728005d803
Victor Woeltjen <victor.woeltjen@nasa.gov>
2014-02-06 14:35:05 -0800

    [Scenario] Add decorator for repository save
    Activity Type and Tag components created via the Create
    menu need to appear in specific repositories by default.
    The added class can be used to decorate their wizard
    UIs to ensure that this happens.

---
+++ scenario/src/main/java/gov/nasa/arc/mct/scenario/component/RepositoryWizardDecorator.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Mission Control Technologies, Copyright (c) 2009-2012, United States Government
+ * as represented by the Administrator of the National Aeronautics and Space 
+ * Administration. All rights reserved.
+ *
+ * The MCT platform is licensed under the Apache License, Version 2.0 (the 
+ * "License"); you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ *
+ * MCT includes source code licensed under additional open source licenses. See 
+ * the MCT Open Source Licenses file included with this distribution or the About 
+ * MCT Licenses dialog available at runtime from the MCT Help menu for additional 
+ * information. 
+ *******************************************************************************/
+package gov.nasa.arc.mct.scenario.component;
+
+import gov.nasa.arc.mct.components.AbstractComponent;
+import gov.nasa.arc.mct.platform.spi.PersistenceProvider;
+import gov.nasa.arc.mct.platform.spi.PlatformAccess;
+import gov.nasa.arc.mct.services.component.ComponentRegistry;
+import gov.nasa.arc.mct.services.component.CreateWizardUI;
+
+import java.util.Collections;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+
+public class RepositoryWizardDecorator extends CreateWizardUI {
+	private CreateWizardUI wizard;
+	private String repositoryID;
+	
+	public AbstractComponent createComp(ComponentRegistry comp,
+			AbstractComponent parentComp) {
+		AbstractComponent created = wizard.createComp(comp, parentComp);
+		
+		// Also persist the created component to the designated repository
+		PersistenceProvider persistence = PlatformAccess.getPlatform().getPersistenceProvider();
+		AbstractComponent repository = persistence.getComponent(repositoryID);
+		repository.addDelegateComponent(created);
+		repository.save();
+		persistence.persist(Collections.singleton(repository));
+		
+		return created;
+	}
+
+	public JComponent getUI(JButton create) {
+		return wizard.getUI(create);
+	}
+
+	public String toString() {
+		return wizard.toString();
+	}
+}
