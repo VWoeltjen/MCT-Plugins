d6746315286bb14b7b0ef6763035d2a27029f5e4
Victor Woeltjen <victor.woeltjen@nasa.gov>
2014-02-28 12:47:27 -0800

    [Scenario] Add My Templates to My Sandbox

--- scenario/src/main/java/gov/nasa/arc/mct/scenario/component/ScenarioTaxonomy.java
+++ scenario/src/main/java/gov/nasa/arc/mct/scenario/component/ScenarioTaxonomy.java
@@ -29,6 +29,7 @@ import gov.nasa.arc.mct.services.internal.component.ComponentInitializer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -37,68 +38,86 @@ public class ScenarioTaxonomy {
 	private static ResourceBundle bundle = ResourceBundle.getBundle("Bundle"); 
 
 	private List<AbstractComponent> bootstraps = new ArrayList<AbstractComponent>();
-	
+	private ComponentRegistry registry = PlatformAccess.getPlatform().getComponentRegistry();
+
 	public ScenarioTaxonomy() {
-		initializeScenarioTaxonomy();
+		List<AbstractComponent> toPersist = new ArrayList<AbstractComponent>();
+
+		bootstraps.add(initializeMissionTaxonomy(toPersist));
+		toPersist.add(initializeUserTaxonomy(toPersist));
+
+		// Workaround lack of support for introducing bootstrap components with children
+		PlatformAccess.getPlatform().getPersistenceProvider().persist(toPersist);
 	}
 	
 	public Collection<AbstractComponent> getBootstrapComponents() {
 		return bootstraps;
 	}
 	
-	private void initializeScenarioTaxonomy() {
+	private AbstractComponent initializeUserTaxonomy(List<AbstractComponent> toPersist) {
+		String user = PlatformAccess.getPlatform().getCurrentUser().getUserId();
+		AbstractComponent sandbox = PlatformAccess.getPlatform().getMySandbox();
+
+		try {
+			addTemplateRepositories(sandbox, toPersist, "user", user);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return sandbox;
+	}
+	
+	private AbstractComponent initializeMissionTaxonomy(List<AbstractComponent> toPersist) {
 		AbstractComponent mission = initialize(
 				new MissionComponent(),
 				bundle.getString("bdn_mission"),
 				bundle.getString("mission_uuid"),
 				bundle.getString("mission_owner"));
 		
-		ComponentRegistry registry = PlatformAccess.getPlatform().getComponentRegistry();
+		addTemplateRepositories(mission, toPersist, "mission", "*");
 		
-		String wild = "*";
-		String tagRepoId = bundle.getString("prefix_tagrepo") + wild;
-		String typeRepoId = bundle.getString("prefix_typerepo") + wild;
-		String missionTemplatesId = bundle.getString("missiontemplates_uuid");
+		return mission;
+	}
+	
+	private void addTemplateRepositories(AbstractComponent parent, 
+			List<AbstractComponent> toPersist,
+			String key, String suffix) {
+		String tagRepoId = bundle.getString("prefix_tagrepo") + suffix;
+		String typeRepoId = bundle.getString("prefix_typerepo") + suffix;
+		String templatesId = bundle.getString("prefix_templates") + suffix;
 		
 		// Tag and Type repos may already exist
 		AbstractComponent tagRepo = registry.getComponent(tagRepoId);
 		AbstractComponent typeRepo = registry.getComponent(typeRepoId);
-		AbstractComponent missionTemplates = registry.getComponent(missionTemplatesId);
-
-		List<AbstractComponent> toPersist = new ArrayList<AbstractComponent>();
+		AbstractComponent templates = registry.getComponent(templatesId);
 		
 		if (tagRepo == null) {
 			toPersist.add(tagRepo = initialize(new TagRepositoryComponent(),
-					bundle.getString("bdn_missiontags"),
+					bundle.getString("bdn_" + key + "tags"),
 					tagRepoId, 
-					wild));					
+					suffix));					
 		}
 		
 		if (typeRepo == null) {
 			toPersist.add(typeRepo = initialize(new CostRepositoryComponent(),
-					bundle.getString("bdn_missiontypes"),
+					bundle.getString("bdn_" + key + "types"),
 					typeRepoId, 
-					wild));					
+					suffix));					
 		}
 		
-		
-		if (missionTemplates == null) {
-			toPersist.add(missionTemplates = initialize(
+		if (templates == null) {
+			toPersist.add(templates = initialize(
 					new ScenarioTaxonomyComponent(),
-					bundle.getString("bdn_missiontemplates"),
-					missionTemplatesId,
+					bundle.getString("bdn_" + key + "templates"),
+					templatesId,
 					bundle.getString("mission_owner")));
 		}
 		
-		missionTemplates.addDelegateComponents(Arrays.asList(typeRepo, tagRepo));
-
-		// Workaround lack of support for introducing bootstrap components with children
-		PlatformAccess.getPlatform().getPersistenceProvider().persist(toPersist);
+		templates.addDelegateComponents(Arrays.asList(typeRepo, tagRepo));
 		
-		mission.addDelegateComponent(missionTemplates);
-		
-		bootstraps.add(mission);	
+		parent.addDelegateComponents(0, Collections.singleton(templates));	
 	}
+
 	
 	private AbstractComponent initialize(
 			AbstractComponent ac, String bdn, String id, String owner) {
